bc90408ea96008fb2f98f1bf2ace1412a650af28
onixcodeAB <onixcode.AB@gmail.com>
2025-06-02 14:07:32 -0400

    feat: Handle user creation webhook and insert into database

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,18 +1,29 @@
+import { query } from "@/lib/db";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
-
-    // Do something with payload
-    // For this guide, log payload to console
     const { id } = evt.data;
     const eventType = evt.type;
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`
-    );
-    console.log("Webhook payload:", evt.data);
+    
+    if (eventType === "user.created") {
+      // Extrac relevant fields
+      const userId = id;
+      const email = evt.data.email_addresses?.[0]?.email_address ?? null;
+      const fullName = `${evt.data.first_name ?? ""} ${
+        evt.data.last_name ?? ""
+      }`.trim();
+
+      // Insert into DB
+      await query(
+        `INSERT INTO user (id, email, name) VALUES ($1, $2, $3)
+        ON CONFLICT (id) DO NOTHING`,
+        [userId, email, fullName]
+      );
+      console.log(`Inserted new user ${userId}: ${fullName} (${email})`);
+    }
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
