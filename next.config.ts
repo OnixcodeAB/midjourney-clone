fa6694ae61748ae03a5d002fe74163b2bbe3bcd5
onixcodeAB <onixcode.AB@gmail.com>
2025-06-02 08:00:09 -0400

    feat: Update image configuration to use remote patterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['res.cloudinary.com'], // 👈 Add this line
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ], // 👈 Add this line
   },
 };
 
