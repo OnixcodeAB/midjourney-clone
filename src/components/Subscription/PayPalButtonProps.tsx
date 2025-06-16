9ccba12df5401f9eeedb8161115a1d1256580a44
onixcodeAB <onixcode.AB@gmail.com>
2025-06-16 12:52:14 -0400

    feat: agregar componente PayPalButton para suscripciones y pagos

---
+++ src/components/Subscription/PayPalButtonProps.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect, useRef } from "react";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { PayPalSubscribeButton } from "./PayPalSubscribeButton";
+import { PayPalSubscriptionUpdatedButton } from "./PayPalSubscriptionUpdatedButton";
+
+interface PayPalButtonProps {
+  isUpdate: boolean;
+  currentSubsCriptionId?: string;
+  newPlanId: string;
+  planId?: string;
+}
+
+export function PayPalButton({
+  isUpdate,
+  currentSubsCriptionId,
+  newPlanId,
+  planId,
+}: PayPalButtonProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant={"default"}>Subscribe</Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="p-4 w-fit">
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Choose a subscription payment method
+          </AlertDialogTitle>
+          <AlertDialogDescription className="p-2">
+            Please use one button below to complete your subscription process.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        {isUpdate && currentSubsCriptionId ? (
+          <PayPalSubscriptionUpdatedButton
+            currentSubsCriptionId={currentSubsCriptionId}
+            newPlanId={newPlanId}
+          />
+        ) : (
+          planId && <PayPalSubscribeButton planId={planId} isSelected={false} />
+        )}
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
