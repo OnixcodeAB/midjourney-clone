b3bda15bea7a82c3150a4f15fe38ca65aaaa4511
onixcodeAB <onixcode.AB@gmail.com>
2025-06-25 15:05:33 -0400

    feat: agregar usuario actual para personalizar clave de caché en API

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,13 +1,17 @@
 // app/api/create/route.ts
 import { query } from "@/lib/db";
 import { getCached, cacheResult } from "@/lib/redis";
+import { currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// Cache key and TTL constants
-const IMAGES_CACHE_KEY = "api:create:all";
-const CACHE_TTL = 60 * 5; // 5 minutes in seconds
-
 export async function GET() {
+  // Get the current user from Clerk
+  const user = await currentUser();
+
+  // Cache key and TTL constants
+  const IMAGES_CACHE_KEY = `api:create:${user?.id}`;
+  const CACHE_TTL = 60 * 5; // 5 minutes in seconds
+
   // Try to get from cache first
   const cachedImages = await getCached<any[]>(IMAGES_CACHE_KEY);
   if (cachedImages) {
@@ -17,7 +21,8 @@ export async function GET() {
   // If not in cache, query the database
   // and cache the result
   try {
-    const { rows: images } = await query(`
+    const { rows: images } = await query(
+      `
       SELECT
         i.id,
         i.url,
@@ -36,8 +41,11 @@ export async function GET() {
           '{}'
         )               AS tags
       FROM "Image" i
+      WHERE i.user_id = $1
       ORDER BY i."createdat" DESC;
-    `);
+    `,
+      [user?.id]
+    );
 
     // Cache the images for future requests
     await cacheResult(IMAGES_CACHE_KEY, CACHE_TTL, images);
