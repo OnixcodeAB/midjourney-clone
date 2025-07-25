4538f5ef90553986f5b22066caafb3a5b400a595
onixcodeAB <onixcode.AB@gmail.com>
2025-07-25 09:34:56 -0400

    feat: agregar componente DynamicAspectImage y ajustar la página de encuestas

--- src/app/surveys/page.tsx
+++ src/app/surveys/page.tsx
@@ -1,17 +1,55 @@
-import { SubscriptionPlans } from "@/components/Subscription/SubscriptionPlans";
-import { query } from "@/lib/db";
+import { DynamicAspectImage } from "@/components/surveys/DynamicAspectImage";
+import { getImageData } from "../actions/image/getImageData";
+
+const response = await getImageData();
+
+let images: ImageExplorePage[] = [];
+if (response.error) {
+  console.error("Error fetching images:", response.error);
+} else {
+  images = response.imagesWithLikes || [];
+  //console.log(images);
+}
+
+function splitIntoColumns<T>(data: T[], columns: number): T[][] {
+  const result: T[][] = Array.from({ length: columns }, () => []);
+  data.forEach((item, index) => {
+    result[index % columns].push(item);
+  });
+  return result;
+}
 
 export default async function SurveysPage() {
   //console.log("Fetched Plans:", Plans);
+  const columns = splitIntoColumns(images, 5);
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-background">
-        {/* parent */}
-      <div className="w-full animate-rotate-border max-w-sm rounded-lg card-wrapper">
-        {/* content */}
-        <div className="p-10 rounded-lg bg-gray-100 dark:bg-gray-800 text-center">
-          <h2 className="text-2xl font-bold ">Surveys Coming Soon!</h2>
-        </div>
-      </div>
+    <div className="flex justify-center gap-1 p-4">
+      {columns.map((col, colIndex) => {
+        return (
+          <div key={colIndex} className="flex flex-col gap-1">
+            {col.map((img, imgIndex) => {
+              let cornerClass = "";
+
+              if (colIndex === 0 && imgIndex === 0) {
+                cornerClass = "rounded-tl-lg";
+              }
+
+              if (colIndex === columns.length - 1 && imgIndex === 0) {
+                cornerClass = "rounded-tr-lg";
+              }
+
+              return (
+                <DynamicAspectImage
+                  key={img.id}
+                  src={img.url}
+                  alt={img.alt}
+                  className={cornerClass}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
